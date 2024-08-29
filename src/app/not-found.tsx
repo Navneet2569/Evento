9a8bcbe115efc5e2018d2a00e6c3f02ecc533cc9
Navneet Singh <116500700+Navneet2569@users.noreply.github.com>
2024-08-29 21:29:17 +0530

    Initial commit with Home Page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+function NotFound() {
+  return (
+    <main className="flex items-center justify-center mx-auto my-auto text-4xl">
+      We could not find that page
+    </main>
+  );
+}
+
+export default NotFound;
