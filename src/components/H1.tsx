bb381bd9d7ef3da77ab754b9c4ecf7b94923e379
Navneet Singh <116500700+Navneet2569@users.noreply.github.com>
2024-09-03 23:27:53 +0530

    Events Page Styled

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,8 +1,17 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-function H1({ children }: { children: React.ReactNode }) {
+function H1({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) {
   return (
-    <h1 className="text-3xl lg:text-6xl font-bold tracking-tight">
+    <h1
+      className={cn("text-3xl lg:text-6xl font-bold tracking-tight", className)}
+    >
       {children}
     </h1>
   );
